28637b376c164dc433c74d1e2d0a2cee9e4416a2
Devanshu Mishra <mishradev1910@gmail.com>
2020-08-23 16:20:03 +0530

    New Button is working

---
+++ Scripts/NewList.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class NewList : MonoBehaviour
+{
+	public GameObject ListContainer;        //Stores a reference to the List Container
+	public GameObject SampleList;           //SampleList gameobject that can be modified
+
+	public void CreateList()
+	{
+		GameObject createdList = Instantiate(SampleList);
+		createdList.transform.SetParent(ListContainer.transform);    //Set the parent
+		SendNewButtonToLast();
+	}
+
+	void SendNewButtonToLast()
+	{
+		this.transform.SetParent(null);
+		this.transform.SetParent(ListContainer.transform);
+	}
+}
