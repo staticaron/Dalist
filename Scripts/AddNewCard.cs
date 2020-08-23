f13a0cc6cd35b53be8d885f076b9370f277fafc6
Devanshu Mishra <mishradev1910@gmail.com>
2020-08-23 17:57:00 +0530

    Card Adder added

---
+++ Scripts/AddNewCard.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class AddNewCard : MonoBehaviour
+{
+	public GameObject cardContainer;
+	public GameObject sampleCard;
+
+	public void Clicked()
+	{
+		GameObject createdCard = Instantiate(sampleCard);
+		createdCard.transform.SetParent(cardContainer.transform);
+	}
+}
---
+++ Scripts/Card.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Card : MonoBehaviour
+{
+	private TMP_InputField thisCard;
+
+	void Awake()
+	{
+		thisCard = GetComponent<TMP_InputField>();
+	}
+
+	public void Clicked()
+	{
+		thisCard.enabled = true;
+		thisCard.ActivateInputField();
+	}
+}
